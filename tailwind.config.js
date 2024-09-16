1c9d1f937266ee81ecca392298745f56db08e77c
Jamil <jfayad@dept-info.crosemont.quebec>
2024-09-15 21:51:40 -0400

    Travailer sur la réactivité du systéme de grid.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
       },
       rotate: {
         '135': '135deg',
-      }
+      },
+      gridTemplateRows: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      screens: {
+        'bigPhone': {'max': '640px'},
+        'phone' : {'max': '450'},
+      },
+      gridRow: {
+        'span-1/2': 'span 0.5 / span 0.5',
+        'span-1.5': 'span 1.5 / span 1.5',
+      },
     },
   },
   plugins: [],
