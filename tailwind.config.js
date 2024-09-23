60f3a70f3b074b29026d43b6ced35720d737f2bf
Jamil <jfayad@dept-info.crosemont.quebec>
2024-09-23 19:30:06 -0400

    test 13 de déploiments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-    "!./src/node_modules/**",
-    "!./src/dist/**"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
